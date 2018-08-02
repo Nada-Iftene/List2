83547b0a3c2d75f63f270988f16e6276103ecb7c
Nada-Iftene <32202745+Nada-Iftene@users.noreply.github.com>
2018-08-02 09:24:00 +0200

    Add files via upload

---
+++ List.java
@@ -0,0 +1,12 @@
+
+public class List {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+java.util.List<String> nameList = new java.util.ArrayList<String>();
+nameList.add("zineb");
+nameList.add("nada");
+System.out.println(nameList);
+	}
+
+}
